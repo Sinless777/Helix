9d2c47c40798ba9ca313473086cfb01a0284fdce
Sinless777 <tapierce@sinlessgamesllc.com>
2025-05-14 13:26:37 -0600

    refactor: remove export task and adjust output configuration in Next.js

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -12,7 +12,7 @@ const nxOptions: WithNxOptions = {
 const baseConfig: NextConfig = {
   nx: nxOptions,
   crossOrigin: 'anonymous',
-  
+  output: 'export',
   images: {
     remotePatterns: [
       {
