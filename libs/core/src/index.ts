8611ff870e2c878703721a1a8fb6e19397be6379
Sinless777 <tapierce@sinlessgamesllc.com>
2025-05-15 18:23:04 -0600

    feat: initialize core library with Colors, Constants, and Errors modules

---
+++ libs/core/src/index.ts
@@ -0,0 +1,8 @@
+// Import then Export the Colors
+export * from './Colors'
+
+// import and export the Constants
+export * from './Constants'
+
+// import and export the Errors
+export * from './Errors'
