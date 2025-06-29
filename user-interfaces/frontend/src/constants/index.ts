26603d441240b2b855693406dd326f9574f7be09
Sinless777 <tpierce@sinlessgamesllc.com>
2025-06-29 21:08:36 +0000

    feat: add constants exports for about, bot, Docs, header, home, system, and technology

---
+++ user-interfaces/frontend/src/constants/index.ts
@@ -0,0 +1,7 @@
+export * as about from "./about";
+export * as bot from "./bot";
+export * as Docs from "./Docs";
+export * as header from "./header";
+export * as home from "./home";
+export * as system from "./system";
+export * as technology from "./technology";
