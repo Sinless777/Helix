342e06c4d7ba9c0cd22fa0efdf7f3269a60b9787
Sinless777 <tpierce@sinlessgamesllc.com>
2025-07-12 20:12:16 +0000

    feat: add constants and status URLs for Discord and Cloudflare

---
+++ libs/core/src/lib/constants/index.ts
@@ -0,0 +1,55 @@
+/**
+ * Colors
+ */
+export { SystemColors, BotColors, HelixColors } from './colors';
+
+/**
+ * Errors
+ */
+export { ErrorCodes } from './Errors';
+export type { errCodes } from './Errors';
+
+/**
+ * Frontend
+ */
+export {
+  AboutContent, type AboutSection, KeyValuePillars,
+  RoadMap,
+  InnovationCriteria,
+  type Phase,
+  type Task,
+  type APACitation,
+  type CitationPageData,
+  Citations,
+  type NavItem,
+  type NavSection,
+  navSections,
+  type Page,
+  type HeaderProps,
+  headerProps,
+  pages,
+  nodes,
+  edges,
+  DataStorageCards,
+  DevelopmentCards,
+  FrameworksCards,
+  InfrastructureCards,
+  MetricsExportersCards,
+  NetworkingCards,
+  ObservabilityCards,
+  ProgrammingLanguagesCards,
+  SecurityCards,
+  ToolsCards,
+  MainTheme,
+  DocsTheme
+} from './frontend';
+
+/**
+ * Types
+ */
+export type { CardProps, ListItemProps } from './types';
+
+/**
+ * Status Urls
+ */
+export { StatusUrls } from './status-urls';
